2ef1e55f8ab34812dc5a3afe3fab78be241559fc
adriano.santos <adriano.santos@squadra.com.br>
2019-05-30 09:25:00 -0300

    exercicio parte 02 -01

---
+++ exercicio_parte_02_01/exercicio_parte_02_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_parte_02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sucessor = 0;
+
+            Console.Write("Digite o Número: ");
+            sucessor = int.Parse(Console.ReadLine());
+
+            sucessor += 1;
+
+            Console.WriteLine("O sucessor do Número Digitado é: {0}", sucessor);
+        }
+    }
+}
